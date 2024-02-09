cebb5f1e82ecc1adcae56842ebdbf1d577530fb6
CrazyAmphibian <0crazyamphibian@gmail.com>
2024-02-09 02:20:42 -0800

    changed cozy efffect
    more in line with the intention with the dynamic damage resistance.

--- files/effects/cocoa.lua
+++ files/effects/cocoa.lua
@@ -6,7 +6,7 @@ function damage_about_to_be_received( damage, x, y, ent_thats_responsible, crit_
 	local damagemodel = EntityGetFirstComponent( player, "DamageModelComponent" )
 	if not damagemodel then return damage*mult,crit_chance end
 	
-	mult=mult*(tonumber( ComponentGetValue2( damagemodel, "hp" ) )-damage)/tonumber( ComponentGetValue2( damagemodel, "max_hp"))<=.5 and .75 or 1 --20% to 40% damage reduction under 1/2 health. operates like the brass beast from TF2, so if the hit would reduce you to that amount.
+	mult=mult*(tonumber( ComponentGetValue2( damagemodel, "hp" ) )-damage*.8)/tonumber( ComponentGetValue2( damagemodel, "max_hp"))<=.5 and .75 or 1 --20% to 40% damage reduction under 1/2 health. operates like the brass beast from TF2, so if the hit would reduce you to that amount.
 
 	--print("called about")
     return damage*mult, crit_chance
