b20653dd4ebca4399a3d3e1732e2301c1f53e71a
CrazyAmphibian <0crazyamphibian@gmail.com>
2025-09-16 18:17:59 -0700

    fix player lunch stealing

--- files/pixel_scenes/alchemist_house/potion_looker.lua
+++ files/pixel_scenes/alchemist_house/potion_looker.lua
@@ -5,23 +5,26 @@ local x, y = EntityGetTransform( entity_id )
 local ents=EntityGetInRadius(x, y, 20)--EntityGetInRadius(x,y,60)
 
 local mats_spawn_spells={
-magic_liquid_berserk = "BERSERK_FIELD",
-magic_liquid_faster_levitation_and_movement = "LEVITATION_FIELD",
-magic_liquid_movement_faster = "LEVITATION_FIELD",
-magic_liquid_faster_levitation = "LEVITATION_FIELD",
-magic_liquid_teleportation = "TELEPORTATION_FIELD",
-magic_liquid_unstable_teleportation = "TELEPORTATION_FIELD",
-magic_liquid_protection_all = "SHIELD_FIELD",
-blood_cold = "FREEZE_FIELD",
-magic_liquid_polymorph = "POLYMORPH_FIELD",
-magic_liquid_random_polymorph = "CHAOS_POLYMORPH_FIELD",
-magic_liquid_unstable_polymorph = "CHAOS_POLYMORPH_FIELD",
+	magic_liquid_berserk = "BERSERK_FIELD",
+	magic_liquid_faster_levitation_and_movement = "LEVITATION_FIELD",
+	magic_liquid_movement_faster = "LEVITATION_FIELD",
+	magic_liquid_faster_levitation = "LEVITATION_FIELD",
+	magic_liquid_teleportation = "TELEPORTATION_FIELD",
+	magic_liquid_unstable_teleportation = "TELEPORTATION_FIELD",
+	magic_liquid_protection_all = "SHIELD_FIELD",
+	blood_cold = "FREEZE_FIELD",
+	magic_liquid_polymorph = "POLYMORPH_FIELD",
+	magic_liquid_random_polymorph = "CHAOS_POLYMORPH_FIELD",
+	magic_liquid_unstable_polymorph = "CHAOS_POLYMORPH_FIELD",
 }
 
+if ModIsEnabled("grahamsperks") then
+	mats_spawn_spells.graham_tele_chaotic ="GRAHAM_TRANSLOCATION"
+end
 
 for i=1,#ents do
 	local entid=ents[i]
-	if EntityGetRootEntity(entid) == entid then
+	if EntityGetRootEntity(entid) == entid and EntityHasTag(entid, "item_pickup") then
 		local invcomp=EntityGetFirstComponent(entid, "MaterialInventoryComponent")
 		if invcomp and invcomp~=0 then
 			local mats=ComponentGetValue2(invcomp,"count_per_material_type")
@@ -40,6 +43,7 @@ for i=1,#ents do
 							AddMaterialInventoryMaterial(entid, _MATERIALNAME , mats[m]-1000 ) 
 							EntityLoad("data/entities/particles/image_emitters/potion_effect.xml", x, y)
 							CreateItemActionEntity(spell,x,y-20)
+							return
 						end
 					end
 				end
