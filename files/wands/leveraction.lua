24b987e0a9b32f769b69d239c2496abfde499fb5
CrazyAmphibian <0crazyamphibian@gmail.com>
2026-02-02 19:13:53 -0700

    wand buff

--- files/wands/leveraction.lua
+++ files/wands/leveraction.lua
@@ -11,3 +11,7 @@ AddGunAction( entity_id, "EL_ALCHEMYBUCKSHOT" )
 
 AddGunAction( entity_id, "EL_ALCHEMYDAMAGEPLUS" )
 AddGunAction( entity_id, "EL_ALCHEMYBUCKSHOT" )
+
+
+AddGunActionPermanent( entity_id, "HITFX_CRITICAL_OIL" )
+AddGunActionPermanent( entity_id, "HITFX_CRITICAL_OIL" )
